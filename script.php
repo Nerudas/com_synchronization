483fb5fa02b58309a85e7adf8a9e29ec30b7ce3a
Septdir <septdir@gmail.com>
2018-03-16 12:21:57 +0300

    add cleanup

---
+++ script.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package    Synchronization Component
+ * @version    1.0.1
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+class com_SynchronizationInstallerScript
+{
+	/**
+	 * Runs right after any installation action is preformed on the component.
+	 *
+	 * @param  string $type      Type of PostFlight action. Possible values are:
+	 *                           - * install
+	 *                           - * update
+	 *                           - * discover_install
+	 * @param         $parent    Parent object calling object.
+	 *
+	 * @return bool
+	 *
+	 * @since    1.0.1
+	 */
+	function preflight( $type, $parent ) {
+		$path = JPATH_ADMINISTRATOR . '/components/com_synchronization';
+		if (JFolder::exists($path)) {
+			JFolder::delete($path);
+		}
+		return true;
+	}
+
+}
