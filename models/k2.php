8ed9cbaee46372dd4d1c790b396abe267bbfbad7
Septdir <septdir@gmail.com>
2018-06-29 15:00:59 +0300

    Remove platon

---
+++ models/k2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package    Synchronization Component
+ * @version    1.0.4
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
